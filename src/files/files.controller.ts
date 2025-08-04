57b0e1b7902eb10a0b8db09e0b6770c4f9f8bb32
Delio Yunior <yunior0716@gmail.com>
2025-08-03 23:50:09 -0400

    Added file upload validation

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -6,14 +7,25 @@ import {
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+
+    return {
+      fileName: file.originalname,
+    };
   }
 }
